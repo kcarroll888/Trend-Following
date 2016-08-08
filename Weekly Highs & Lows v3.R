47020235d673c06e24326ddef572258e06714b7b
Kurt Carroll <kurt.carroll@rocketmail.com>
2016-08-08 21:23:54 +0100

    Latest version of trend following code
    * Error handling when no data returned in call to getYahooData
    * Graphing function to analyse output by Industry
    * Latest version of equity universes and ETF universe

--- Weekly Highs & Lows v3.R
+++ Weekly Highs & Lows v3.R
@@ -11,6 +11,10 @@
 # Libraries Needed
 library(quantmod)   # for getting & working with equity data
 library(dplyr)
+library(ggplot2)
+
+setDefaults(chartSeries, up.col="green", dn.col="blue",
+            theme="black")
 
 toWeekly <- function(xtsDF, keepLast=FALSE) {
   # xts object with Dates as the row index
@@ -85,46 +89,8 @@ getLastFri <- function(dte){
                      "Friday"=0)
   dte <- format(dte - subtract, format="%Y%m%d")
   
-  # Check date isn't weekend
-  # if(weekdays(dte)=="Saturday"){
-  #    dte <- format(dte - 1, format="%Y%m%d")
-  
-  #} else if(weekdays(dte)=="Sunday"){
-  #    dte <- format(dte - 2, format="%Y%m%d")
-  
-  #} else dte <- format(dte, format="%Y%m%d")
-  
   # Convert and return
   as.numeric(dte)
-}
-
-returnSD <- function(symbol){
-    # Calculate the change in % return week to week and comapre to
-    # standard deviation of the 40 week average of return
-    
-    # How many standard deviations is the last weeks return compared to past 40 weeks?
-    
-    endDate <- getLastFri(Sys.Date())
-    startDate <- as.Date(format(endDate), format="%Y%m%d") - 1120 # 40 * 28
-    startDate <- getLastFri(startDate)
-    
-    symD <- getYahooData(symbol, startDate, endDate, adjust=TRUE)
-    
-    symW <- toWeekly(symD, keepLast=TRUE)
-    
-    returns <- dailyReturn(symW)
-    
-    # only need last 40 occurances, if we have them
-    if(nrow(symW) > 40){
-        symW <- symW[(nrow(symW)-40):nrow(symW)]
-    }
-    
-    # standard deviation of the weekly returns
-    sdRtn <- sd(returns, na.rm=TRUE)
-    
-    # how does the last week return compare to the sd?
-    lastRtn <- abs(tail(returns, 1))[[1]]
-    lastRtn/sdRtn
 }
 
 chartBreakout <- function(symbol, period=40, timeF="W", endDate=NA) {
@@ -157,7 +123,7 @@ chartBreakout <- function(symbol, period=40, timeF="W", endDate=NA) {
     
     # Convert to weekly format
     symbolData <- toWeekly(symbolData[,c("Open", "High", "Low", "Close", "Volume")],
-                           keepLast=TRUE)
+                           keepLast=FALSE)
     
   } else if(timeF=="D"){
     # Work out the how much data to get for the daily period specified
@@ -171,12 +137,10 @@ chartBreakout <- function(symbol, period=40, timeF="W", endDate=NA) {
   } else stop("Timeframe must be 'W' for weekly or 'D' for daily")
   
   # Plot the chart
-  setDefaults(chartSeries, up.col="green", dn.col="blue",
-              theme="black")
   chartSeries(symbolData, name=symbol, TA=NULL)
   
-  # Add Donchian Channel 1 means on main chart
-  don <- DonchianChannel(symbolData[,2:3], n=period)
+  # Add Donchian Channel '1' means on main chart
+  don <- DonchianChannel(symbolData[,"Close"], n=period)
   addTA(don[,c("high", "low")], on = 1)
   
   # addATR(n=period)
@@ -198,132 +162,214 @@ getStopLoss <- function(series, lookBack) {
   # Calculate the ATR
   atr <- ATR(series[, c("High", "Low", "Close")], n=lookBack)
   
-  # Return the most recent 2 x ATR in the data frame
+  # Return the most recent ATR, doubled, in the data frame
   as.numeric(2 * last(atr$atr))
 }
 
-getBreakouts <- function(fileName, lookBack=40) {
-    # Get the weekly breakouts and breakdowns of the stocks contained in 'filename'
-    # based on looking back 'lookBack' weeks
-    
-    # Expects file to be type CSV in the following format;
-    #   Bloomberg Code - Stocks Bloomberg Ticker
-    #   Yahoo Code - Stocks Yahoo Ric
-    
-    # Returns the stocks making new highs/lows over the weekly 'lookBack' supplied
-    # Read in the file of tickers
-    stockList <- read.csv(fileName, stringsAsFactors=FALSE)
-    
-    numberStocks <- nrow(stockList)
-    
-    # And how far we look back will determine start date of
-    # data retrieval. Make sure the dates are weekdays otherwise
-    # yahoo returns errors. Find the last Friday
-    
-    endD <- getLastFri(Sys.Date())
-    endDate <- as.character(endD)
-    endDate <- as.Date(endDate, format="%Y%m%d")
-    
-    startDate = endDate - (lookBack * 14)
-    startD <- format(startDate, format="%Y%m%d") # Convert start date to character
-    startD <- as.numeric(startD)                 # and then numeric for quantmod call
-    
-    # Create dataframes to store the highs & lows
-    newHighLow <- data.frame(1,1,1,1,1)
-    colnames(newHighLow) <- c("Yahoo", "Bloomberg", "Type", "ATR", "Close")
-    rowHL <- 1
-    
-    # Loop through all the stocks and check if they've
-    # reached a high or low compared to current observation
-    for(i in 1:numberStocks){
-        
-        # Get current symbol
-        curSymbol <- stockList[i,2]
-        print(curSymbol)
-        
-        # Get daily data for current symbol from Yahoo
-        curStockData <- getYahooData(curSymbol, startD, endD,
-                                     adjust=TRUE)
-        
-        # Store todays date to compare against
-        curDateRow <- tail(curStockData, 1)
-        
-        # Convert this dataframe to weekly data
-        curStockData <- toWeekly(curStockData[,c("Open", "High", "Low", "Close", "Volume")],
-                                 keepLast=TRUE)
-        # names(curStockData) <- c("Open", "High", "Low", "Close")
-        
-        # Remove this row from the series & save to compare
-        curStockData <- curStockData[-nrow(curStockData),]
-        
-        # Now test for highs & lows of the previous weeks
-        # First check if have enough data to look back 40 weeks
-        sr <- ifelse(nrow(curStockData) > 40, nrow(curStockData) - 40, 1)
-        curMax <- unique(seriesHi(curStockData$Close[sr:nrow(curStockData)]))
-        curMin <- unique(seriesLo(curStockData$Close[sr:nrow(curStockData)]))
-        
-        # Does current weekly close make a new high or low?
-        if(as.numeric(curDateRow$Close) >= as.numeric(curMax)){
-            
-            # Yes so save the ticker in the output data frame
-            newHighLow[rowHL, 1:3] <- c(curSymbol,
-                                        stockList[i, "Bloomberg.Code"],
-                                        "High")
-            
-            newHighLow$Close[rowHL] <- as.numeric(curDateRow$Close)
-            
-            # Put the stop loss in the data frame
-            newHighLow$ATR[rowHL] <- getStopLoss(curStockData, lookBack)
-            
-            # Ready for the next row of output dataframe
-            rowHL <- rowHL + 1
-            
-        } else if(as.numeric(curDateRow$Close) <= as.numeric(curMin)){
-            
-            # If its made a new low then save in output data frame
-            newHighLow[rowHL, 1:3] <- c(curSymbol,
-                                        stockList[i, "Bloomberg.Code"],
-                                        "Low")
-            
-            newHighLow$Close[rowHL] <- as.numeric(curDateRow$Close)
-            
-            # Put the stop loss in the data frame
-            newHighLow$ATR[rowHL] <- getStopLoss(curStockData, lookBack)
-            
-            # Ready for the next row of output dataframe
-            rowHL <- rowHL + 1
-        }
-    }
-    
-    return(newHighLow)
+returnSD <- function(symbol){
+  # Calculate the change in % return week to week and comapre to
+  # standard deviation of the 40 week average of return and ATR
+  
+  # How many standard deviations is the last weeks return and ATR compared to past 40 weeks?
+  endDate <- getLastFri(Sys.Date())
+  startDate <- as.Date(format(endDate), format="%Y%m%d") - 1120 # 40 * 28
+  startDate <- getLastFri(startDate)
+  
+  symD <- getYahooData(symbol, startDate, endDate, adjust=TRUE)
+  
+  symW <- toWeekly(symD, keepLast=F)
+  
+  ret <- dailyReturn(symW)
+  atr <- ATR(symW[,c("High", "Low", "Close")])[,2]
+  
+  # only need last 40 occurances, if we have them
+  if(nrow(symW) > 40){
+    symW <- symW[(nrow(symW)-40):nrow(symW)]
+  }
+  
+  # standard deviation of the weekly returns
+  zRtn <- scale(ret)
+  zATR <- scale(atr)
+  
+  # Return the last std dev readings of Return & ATR, i.e this week
+  r <- cbind(Symbol=symbol, Return=last(zRtn), ATR=last(zATR))
+  return(r)
 }
 
-weeklyRun <- function(risk=500){
+stopLossTriggered <- function(Tickers){
+  # Given a vector of Tickers, find the standard deviation of the last
+  # recording of either "Return" and "ATR".
+  t <- lapply(Tickers, returnSD)
+  
+  # Extract each element of the list returned and turn into a datframe
+  sym <- sapply(t, function(l) l[1])
+  ret <- sapply(t, function(r) as.numeric(r[2]))
+  atr <- sapply(t, function(a) as.numeric(a[3]))
+
+  data.frame(Symbol=sym, Return=ret, ATR=atr)
+}
+
+isNewHighLow <- function(ticker, lookBack=40, ed=Sys.Date()){
+  # Function to remove the for loop of testing for a breakout
+  # For the given Yahoo ticker, lookBack period and end date test to see
+  # if a new weekly High/Low was made.
+  # Returns a dataframe with type of breakout (High/Low), the close and stop
+  # loss if new high/low was made. Otherwise returns empty dataframe
+  
+  print(ticker)
+  
+  # Create a result vector to return after function has been run
+  result <- vector("list", 1)
+  
+  # Work out start and end dates & prepare the format for quantmod functions
+  endD <- getLastFri(ed)
+  endDate <- as.character(endD)
+  endDate <- as.Date(endDate, format="%Y%m%d")
+  
+  startDate = endDate - (lookBack * 10)   # Enough daily data to convert into weeks
+  startD <- format(startDate, format="%Y%m%d") # Convert start date to character
+  startD <- as.numeric(startD)                 # and then numeric for quantmod call
+  
+  # Get the data from Yahoo
+  curStockData <- tryCatch(getYahooData(ticker, startD, endD, adjust=TRUE),
+                           error = function(e){
+                             "error"
+                           })
+  
+  if(curStockData=="error"){
+    print("skipped")
+    return(c(ticker, "skip", "skip", "skip"))
+  }
+  
+  # Store todays date to compare against
+  curDateRow <- tail(curStockData, 1)
+  
+  # Convert this dataframe to weekly data
+  curStockData <- toWeekly(curStockData[,c("Open", "High", "Low", "Close", "Volume")],
+                           keepLast=TRUE)
+  
+  # Remove this row from the series & save to compare
+  curStockData <- curStockData[-nrow(curStockData),]
+  curStockData$Close <- as.numeric(curStockData$Close)
+  
+  # Now test for highs & lows of the previous weeks
+  # First check if have enough data to look back 40 weeks
+  sr <- ifelse(nrow(curStockData) > 40, nrow(curStockData) - 40, 1)
+  curMax <- unique(seriesHi(curStockData$Close[sr:nrow(curStockData)]))
+  curMin <- unique(seriesLo(curStockData$Close[sr:nrow(curStockData)]))
+  
+  # Does current weekly close make a new high or low?
+  if(curDateRow$Close >= curMax){
+    
+    # Yes so save the ticker in the output data frame
+    # Also calculate ATR and Weekly Return
+    result <- c(ticker, "High", curDateRow$Close,
+                getStopLoss(curStockData, lookBack))
+    
+  } else if(curDateRow$Close <= curMin){
+    
+    # If its made a new low then save in output data frame
+    result <- c(ticker, "Low", curDateRow$Close,
+                getStopLoss(curStockData, lookBack))
+  } else {
+    # No new high or low made. Return the ticker and empty values
+    result <- c(ticker, NA, NA, NA)
+  }
+
+  return(result)
+}
+
+breakOuts <- function(fileName, lookBack=40, endDate){
+  # read the file of Yahoo tickers
+  l <- read.csv(fileName, stringsAsFactors = F)
+  
+  # test for a breakout in each
+  r <- lapply(l$Yahoo, isNewHighLow, lookBack=lookBack, ed=endDate)
+  
+  # flatten into a dataframe
+  df <- data.frame(matrix(unlist(r), nrow=length(r), byrow=T),
+                   stringsAsFactors = F)
+  colnames(df) <- c("Ticker", "Type", "Close", "Stop")
+  
+  # add the Bloomberg & Industry data from the original list
+  df$Bloomberg <- l$Bloomberg
+  df$Industry <- l$Industry
+  
+  # only want entries with results or 'skip'
+  # meaning Yahoo data was incomplete
+  i <- which(df$Type=="High" | df$Type=="Low" | df$Type=="skip")
+  df <- df[i,]
+  
+  # return the final result
+  return(df)
+}
+
+weeklyRun <- function(risk=500, endDate=Sys.Date()){
   # Get the yahoo data and calculate ATR based stop-loss in 2 calls
   # otherwise Yahoo servers stop allowing access
   print("Updating Weekly Breakouts")
-  A <- getBreakouts("Equity Universe A 2015 Sep.csv", 40)
-  B <- getBreakouts("Equity Universe B 2015 Sep.csv", 40)
+  A <- breakOuts("Equity Universe A May 16.csv", 40, endDate)
+  B <- breakOuts("Equity Universe B May 16.csv", 40, endDate)
+  C <- breakOuts("ETF.csv", 40, endDate)
   
   print("Calculating ATR & Risk")
   # Make the ATR a numeric value so can arrange/manipulate
-  A$ATR <- as.numeric(A$ATR)
-  B$ATR <- as.numeric(B$ATR)
+  A$Stop <- as.numeric(A$Stop)
+  B$Stop <- as.numeric(B$Stop)
+  C$Stop <- as.numeric(C$Stop)
   
-  # Combine the two data frames
-  final <- rbind(A, B)
+  # Combine the three data frames
+  Q <- rbind(A, B)
+  final <- rbind(Q, C)
   
   # Work out the volatility risk of the breakout
-  final$Risk <- final$Close / final$ATR
+  final$Risk <- as.numeric(final$Close) / final$Stop
   
   # Work out the pounds per point position size based on £500 (default) risk
-  final$Stake <- risk / final$ATR
+  final$Stake <- risk / final$Stop
   
   # Sort by breakout type (High / Low), Risk and ATR
-  final <- arrange(final, Type, Risk, ATR)
-  unique(final)
+  final <- arrange(final, Type, Risk, Stop)
+  final <- unique(final)
+  row.names(final) <- NULL
   
   # Save the data frame
   print("Saving ideas to New Ideas.csv")
-  write.csv(final, "New Ideas.csv", row.names=FALSE)
+  fileName <- paste0("Weekly Ideas/New Ideas ",getLastFri(Sys.Date()),".csv")
+  write.csv(final, fileName, row.names=FALSE)
+  final
+}
+
+
+# DISPLAYING THE HIGH AND LOW RESULTS
+displayHigh <- function(i){
+  # Requires a dataframe of results from the new results file generated earlier
+  highs <- filter(i, Type=="High")
+  nHigh <- group_by(highs, Industry)
+  nHigh <- summarise(nHigh, number=n())
+
+  # Order by number of breakouts per industry, highest-lowest
+  nHigh$Industry <- factor(nHigh$Industry,
+                           levels=nHigh$Industry[order(nHigh$number, decreasing = T)])
+  # Plot the result
+  ggplot(nHigh, aes(x=Industry, y=number, color=number)) + geom_count() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    xlab("Industry Group") + ylab("Number of New Highs")
+}
+
+displayHL <- function(i, HL="Low"){
+  # Displaying Results Lows
+  lows <- filter(i, Type==HL)
+  ggplot(lows) + geom_bar(mapping=aes(x=Industry)) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    xlab("Industry Group") + ylab(HL)
+  # nLow <- group_by(lows, Industry)
+  # nLow <- summarise(nLow, number=n())
+  # Order by number of breakouts per industry, highest-lowest
+  # nLow$Industry <- factor(nLow$Industry,
+  #                         levels=nLow$Industry[order(nLow$number, decreasing = T)])
+  # Plot the result
+  # ggplot(nLow) + geom_bar(mapping=aes(x=Industry)) +
+  #   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  #   xlab("Industry Group") + ylab(HL)
 }
